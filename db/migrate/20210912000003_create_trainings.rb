95d4c0e0c7153c18091fe04ceee38e00e0e79864
jeremiahtabb <jeremiah.r29q7.tabb@gmail.com>
2021-09-12 21:12:43 -0400

    Draft migrations

---
+++ db/migrate/20210912000003_create_trainings.rb
@@ -0,0 +1,14 @@
+class CreateTrainings < ActiveRecord::Migration[6.1]
+  def change
+    create_table :trainings do |t|
+      t.belongs_to :employee, null: false, foreign_key: true
+      t.belongs_to :task, null: false, foreign_key: true
+      t.integer :trainer
+      t.integer :completion_date
+      t.integer :initial_production_rate
+      t.integer :initial_error_rate
+
+      t.timestamps
+    end
+  end
+end
