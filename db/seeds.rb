758b4625efc36bc0d25c922f1cdbbb54a667208f
jeremiahtabb <jeremiah.r29q7.tabb@gmail.com>
2021-09-16 20:35:06 -0400

    Update Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,19 @@
 puts "🌱 Seeding employees..."
-ee01 = Employee.create!(name: 'a', public_id: 1001)
-ee02 = Employee.create!(name: 'b', public_id: 1002)
-ee03 = Employee.create!(name: 'c', public_id: 1003)
-ee04 = Employee.create!(name: 'd', public_id: 1004)
-ee05 = Employee.create!(name: 'e', public_id: 1005)
-ee06 = Employee.create!(name: 'f', public_id: 1006)
-ee07 = Employee.create!(name: 'g', public_id: 1007)
-ee08 = Employee.create!(name: 'h', public_id: 1008)
-ee09 = Employee.create!(name: 'i', public_id: 1009)
-ee10 = Employee.create!(name: 'j', public_id: 1010)
+ee01 = Employee.create!(name: 'Stella Jaramillo', public_id: 1001)
+ee02 = Employee.create!(name: 'Jago North', public_id: 1002)
+ee03 = Employee.create!(name: 'Selin Rigby', public_id: 1003)
+ee04 = Employee.create!(name: 'Opal Zamora', public_id: 1004)
+ee05 = Employee.create!(name: 'Dillon Walker', public_id: 1005)
+ee06 = Employee.create!(name: 'Mohsin Browne', public_id: 1006)
+ee07 = Employee.create!(name: 'Katelin Hodgson', public_id: 1007)
+ee08 = Employee.create!(name: 'Isobel Beaumont', public_id: 1008)
+ee09 = Employee.create!(name: 'Addison Kline', public_id: 1009)
+ee10 = Employee.create!(name: 'Pheobe Hayden', public_id: 1010)
  
 puts "🌱 Seeding tasks..."
-ta01 = Task.create!(name: 'ta01', public_id: 1001001)
-ta02 = Task.create!(name: 'ta02', public_id: 1002001)
-ta03 = Task.create!(name: 'ta03', public_id: 1003001)
+ta01 = Task.create!(name: 'Antigen Development', public_id: 1001001)
+ta02 = Task.create!(name: 'Harvesting', public_id: 1002001)
+ta03 = Task.create!(name: 'Purification', public_id: 1003001)
 
 puts "🌱 Seeding trainings..."
 Training.create!(employee_id: ee01.id, task_id: ta01.id, trainer: ee01.id, initial_production_rate: 143, initial_error_rate: 10234)
