adf7b666d02f09aff33e4c03b0492c6972fb46ae
jeremiahtabb <jeremiah.r29q7.tabb@gmail.com>
2021-09-19 18:46:28 -0400

    Fix error in trainings_controller index method

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -2,7 +2,8 @@ class TrainingsController < ApplicationController
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
   def index
-    render json: Training.group("task_id").group("error_rate_in_english").order("task_id").order("error_rate_in_english").order("initial_production_rate DESC")
+    # render json: Training.all
+    render json: Training.order("task_id").order("error_rate_in_english").order("initial_production_rate DESC")
   end
   
   def create
