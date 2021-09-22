0326b00f4084b677fa291d851c48fa688eb83182
jeremiahtabb <jeremiah.r29q7.tabb@gmail.com>
2021-09-22 16:57:19 -0400

    last change to taskcontroller

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -2,19 +2,12 @@ class TrainingsController < ApplicationController
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
   def index
-    render json:  Training
-                    # .order("task_id")
-                    # .order("error_rate_in_english")
-                    # .order("initial_production_rate DESC")
-                    .select('tasks.task_name')
-                    .select('employees.name')
-                    # .select("employees.name, tasks.name")
-                    # .select("tasks.name, employees.name")
-                    # .select("employees.name")
-                    .joins(:employee,:task)
-                    # .joins(:task,:employee)
-                    # .joins("INNER JOIN employees ON employees.id = trainings.employee_id")
-                    # .joins("INNER JOIN tasks ON tasks.id = trainings.task_id")
+    render json:  
+      Training
+        .select('tasks.task_name')
+        .select('employees.name')
+        .joins(:employee, :task)
+        .order("task_name")
   end
   
   def create
